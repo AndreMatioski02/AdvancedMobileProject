a1ed15e093e1d27528930051a71b8103ba17a07f
AndreMatioski02 <matioskiandre4@gmail.com>
2025-05-12 17:30:27 -0300

    Adding winner validation

--- app/src/main/java/com/kotlin/advancedmobileproject/TicTacToeBoard.kt
+++ app/src/main/java/com/kotlin/advancedmobileproject/TicTacToeBoard.kt
@@ -18,4 +18,78 @@ class TicTacToeBoard {
             board[row][col].state = player
         }
     }
+
+    fun checkWin(): CellState? {
+        for (i in 0..2) {
+            if (board[i][0].state != CellState.EMPTY &&
+                board[i][0].state == board[i][1].state &&
+                board[i][1].state == board[i][2].state
+            ) {
+                return board[i][0].state
+            }
+
+            if (board[0][i].state != CellState.EMPTY &&
+                board[0][i].state == board[1][i].state &&
+                board[1][i].state == board[2][i].state
+            ) {
+                return board[0][i].state
+            }
+        }
+
+        if (board[0][0].state != CellState.EMPTY &&
+            board[0][0].state == board[1][1].state &&
+            board[1][1].state == board[2][2].state
+        ) {
+            return board[0][0].state
+        }
+
+        if (board[0][2].state != CellState.EMPTY &&
+            board[0][2].state == board[1][1].state &&
+            board[1][1].state == board[2][0].state
+        ) {
+            return board[0][2].state
+        }
+
+        return null
+    }
+
+    fun isBoardFull(): Boolean {
+        return board.all { row -> row.all { it.state != CellState.EMPTY } }
+    }
+
+    fun checkWinWithCoords(): Triple<CellState?, Pair<Int, Int>, Pair<Int, Int>> {
+        for (i in 0..2) {
+            if (board[i][0].state != CellState.EMPTY &&
+                board[i][0].state == board[i][1].state &&
+                board[i][1].state == board[i][2].state
+            ) {
+                return Triple(board[i][0].state, Pair(i, 0), Pair(i, 2))
+            }
+
+            if (board[0][i].state != CellState.EMPTY &&
+                board[0][i].state == board[1][i].state &&
+                board[1][i].state == board[2][i].state
+            ) {
+                return Triple(board[0][i].state, Pair(0, i), Pair(2, i))
+            }
+        }
+
+        if (board[0][0].state != CellState.EMPTY &&
+            board[0][0].state == board[1][1].state &&
+            board[1][1].state == board[2][2].state
+        ) {
+            return Triple(board[0][0].state, Pair(0, 0), Pair(2, 2))
+        }
+
+        if (board[0][2].state != CellState.EMPTY &&
+            board[0][2].state == board[1][1].state &&
+            board[1][1].state == board[2][0].state
+        ) {
+            return Triple(board[0][2].state, Pair(0, 2), Pair(2, 0))
+        }
+
+        return Triple(null, Pair(0,0), Pair(0,0))
+    }
+
+
 }
--- app/src/main/java/com/kotlin/advancedmobileproject/screen/SecondScreen.kt
+++ app/src/main/java/com/kotlin/advancedmobileproject/screen/SecondScreen.kt
@@ -1,6 +1,8 @@
 package com.kotlin.advancedmobileproject.screen
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
@@ -11,11 +13,20 @@ import com.kotlin.advancedmobileproject.Game
 import com.kotlin.advancedmobileproject.R
 import com.kotlin.advancedmobileproject.Screen
 import com.kotlin.advancedmobileproject.TicTacToeBoard
+import kotlin.math.sqrt
 
 class SecondScreen(private val context: Context, game: Game) : Screen(game) {
 
     private val board = TicTacToeBoard()
+    private val trophyIcon: Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.reload)
+
     private var currentPlayer = CellState.X
+
+    private var winner: CellState? = null
+    private var winLine: Pair<Pair<Int, Int>, Pair<Int, Int>>? = null // ((startRow, startCol), (endRow, endCol))
+
+    private var xWins = 0
+    private var oWins = 0
 
     init {
         paint.color = ContextCompat.getColor(context, R.color.primary)
@@ -27,7 +38,7 @@ class SecondScreen(private val context: Context, game: Game) : Screen(game) {
     override fun draw() {
         canvas.drawColor(ContextCompat.getColor(context, R.color.primary))
 
-        val correctHeight = (canvas.height - 200).toFloat()
+        val correctHeight = (canvas.height - 450).toFloat()
 
         val cellWidth = canvas.width / 3f
         val cellHeight = correctHeight / 3f
@@ -45,23 +56,27 @@ class SecondScreen(private val context: Context, game: Game) : Screen(game) {
             for (cell in row) {
                 val centerX = (cell.col + 0.5f) * cellWidth
                 val centerY = (cell.row + 0.5f) * cellHeight
+
+                val metrics = paint.fontMetrics
+                val offsetY = (metrics.ascent + metrics.descent) / 2
+
                 when (cell.state) {
                     CellState.X -> {
-                        paint.color = Color.BLUE
+                        paint.color = Color.WHITE
                         paint.textSize = 100f
-                        canvas.drawText("x", centerX, centerY, paint)
+                        canvas.drawText("x", centerX, centerY - offsetY, paint)
                     }
                     CellState.O -> {
                         paint.color = Color.BLACK
                         paint.textSize = 100f
-                        canvas.drawText("o", centerX, centerY, paint)
+                        canvas.drawText("o", centerX, centerY - offsetY, paint)
                     }
                     CellState.EMPTY -> { }
                 }
             }
         }
 
-        paint.color = Color.BLUE
+        paint.color = Color.WHITE
         paint.strokeWidth = 10f
 
         canvas.drawRect(
@@ -72,29 +87,92 @@ class SecondScreen(private val context: Context, game: Game) : Screen(game) {
             paint
         )
 
-        paint.color = Color.WHITE
-        paint.textSize = 50f
-        canvas.drawText("Restart", canvas.width / 2f, canvas.height - 100f, paint)
+        val iconSize = 80
+
+        val scaledIcon = Bitmap.createScaledBitmap(trophyIcon, iconSize, iconSize, true)
+        canvas.drawBitmap(scaledIcon, canvas.width / 2f - 40, canvas.height - 150f, paint)
+
+        paint.color = Color.BLACK
+        paint.textSize = 70f
+        canvas.drawText("X: $xWins  |  O: $oWins", canvas.width / 2f, canvas.height - 250f, paint)
+
+        if (winner != null) {
+
+            if (winner == CellState.EMPTY) {
+                paint.color = Color.YELLOW
+                canvas.drawText("Empate!", canvas.width / 2f, canvas.height / 2f - 50f, paint)
+            } else {
+                winLine?.let { (start, end) ->
+                    paint.strokeWidth = 10f
+
+                    val cellWidth = canvas.width / 3f
+                    val cellHeight = correctHeight / 3f
+
+                    val startX = (start.second + 0.5f) * cellWidth
+                    val startY = (start.first + 0.5f) * cellHeight
+                    val endX = (end.second + 0.5f) * cellWidth
+                    val endY = (end.first + 0.5f) * cellHeight
+
+                    val dx = endX - startX
+                    val dy = endY - startY
+                    val length = sqrt((dx * dx + dy * dy).toDouble()).toFloat()
+                    val extendFactor = 100f
+
+                    val unitX = dx / length
+                    val unitY = dy / length
+
+                    val extendedStartX = startX - unitX * extendFactor
+                    val extendedStartY = startY - unitY * extendFactor
+                    val extendedEndX = endX + unitX * extendFactor
+                    val extendedEndY = endY + unitY * extendFactor
+
+                    paint.color = Color.YELLOW
+                    canvas.drawLine(extendedStartX, extendedStartY, extendedEndX, extendedEndY, paint)
+                }
+
+            }
+        }
+
     }
 
 
     override fun handleEvent(event: Int, x: Float, y: Float) {
         if (event == MotionEvent.ACTION_DOWN) {
             val cellWidth = canvas.width / 3f
-            val cellHeight = (canvas.height - 200) / 3f
+            val cellHeight = (canvas.height - 450) / 3f
 
             val col = (x / cellWidth).toInt()
             val row = (y / cellHeight).toInt()
 
-            if(y >= (canvas.height - 200)) {
+            if (y >= (canvas.height - 200)) {
                 board.resetBoard()
+                winner = null
+                winLine = null
                 return
             }
+
+            if (winner != null || !board.board.indices.contains(row) || !board.board[row].indices.contains(col)) return
 
             if (board.board[row][col].state == CellState.EMPTY) {
                 board.makeMove(row, col, currentPlayer)
 
-                currentPlayer = if (currentPlayer == CellState.X) CellState.O else CellState.X
+                val (win, start, end) = board.checkWinWithCoords()
+                if (win != null) {
+                    winner = win
+                    winLine = Pair(start, end)
+
+                    when (winner) {
+                        CellState.X -> xWins++
+                        CellState.O -> oWins++
+                        else -> {}
+                    }
+
+                }
+                else if (board.isBoardFull()) {
+                    winner = CellState.EMPTY
+                } else {
+                    currentPlayer = if (currentPlayer == CellState.X) CellState.O else CellState.X
+                }
             }
         }
     }
